cc5e43be845a300c92d6121df0c6f606b1c88d37
Rafael Alcides <rafaelalci10@gmail.com>
2022-02-16 18:58:03 -0300

    atividade Feita

---
+++ lib/topico2/tabscrol.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TabScroll());
+}
+
+class TabScroll extends StatelessWidget {
+  const TabScroll({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final tabs = [
+      "Tab 1",
+      "Tab 2",
+      "Tab 3",
+      "Tab 4",
+      "Tab 5",
+      "Tab 6",
+      "Tab 7",
+      "Tab 8",
+      "Tab 9",
+    ];
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: DefaultTabController(
+        length: tabs.length,
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text("Tab Scroll",
+            style: TextStyle(
+              fontSize: 32
+            ),
+            ),
+            backgroundColor: Colors.deepPurple,
+            bottom: TabBar(
+              indicatorColor: Colors.yellow,
+              isScrollable: true,
+              tabs: [
+                for(final tab in tabs)
+                Tab(
+                  height: 90,
+                  child: Text(tab,
+                  style: const TextStyle(
+                    fontSize: 24,
+                  ),),
+                )
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              for(final tab in tabs)
+              Center(
+                child: Text(tab,
+                style: const TextStyle(
+                  fontSize: 24
+                ),),
+              )
+            ]),
+        ),
+      ),
+    );
+  }
+}
